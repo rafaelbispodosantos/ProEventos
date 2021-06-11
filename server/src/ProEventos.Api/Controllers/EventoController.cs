463891831dfe283acc5204c692cf01d277855cf8
rafael_bispo23@hotmail.com <rafael_bispo23@hotmail.com>
2021-06-11 16:33:13 -0300

    Eventos Get, Post, Put e Delete criando em EventoController

---
+++ server/src/ProEventos.Api/Controllers/EventoController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ProEventos.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {             
+        public EventoController()
+        {            
+        }
+
+        [HttpGet]
+        public string Get()
+        {     
+          return "Exemplo de Get";      
+        }
+        [HttpPost]
+        public string Post()
+        {     
+                return "Exemplo de Post";
+        }
+        [HttpPut("{id}")]
+        public string Put(int id)
+        {
+          return $"Exemplo de put com id = {id}"; 
+               
+        }
+        [HttpDelete]
+        public string Delete(int id)
+        {     
+          return $"Exemplo de put com id = {id}";      
+        }
+    }
+}
